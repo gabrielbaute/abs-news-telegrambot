db3e8c8d07c66db54f1790ef01e553bea3725908
gabrielbaute <gabrielbaute@gmail.com>
2025-05-18 14:39:06 -0400

    Bot de telegram

---
+++ src/bot/__init__.py
@@ -0,0 +1,5 @@
+from src.bot.bot import TelegramBot
+
+__all__ = [
+    "TelegramBot"
+    ]
---
+++ src/bot/bot.py
@@ -0,0 +1,47 @@
+import os
+import random
+from telegram import Bot, InputMediaPhoto
+from telegram.error import TelegramError
+from src.core.audiobookshelf import AudiobookshelfAPI
+from src.config.config import Config
+
+class TelegramBot:
+    def __init__(self):
+        self.bot = Bot(token=Config.TELEGRAM_BOT_TOKEN)
+        self.chat_id = Config.TELEGRAM_CHAT_ID
+
+    async def send_book_of_the_day(self):
+        api = AudiobookshelfAPI()
+        libraries = api.get_libraries()
+        if not libraries:
+            return "❌ No se encontraron bibliotecas."
+
+        books = api.get_all_books(libraries[0]["id"])  # Ajusta el límite
+        if not books:
+            return "❌ No se encontraron libros."
+        
+        random_book = random.choice(books)
+        
+        book_details = api.get_book_details(random_book["id"])  # Primer libro (luego lo haremos aleatorio)
+        if not book_details:
+            return "❌ No se pudieron obtener los detalles del libro."
+
+        formatted = api.format_book_message(book_details)
+        
+        try:
+            if formatted["cover_url"]:
+                await self.bot.send_photo(
+                    chat_id=self.chat_id,
+                    photo=formatted["cover_url"],
+                    caption=formatted["message"],
+                    parse_mode="Markdown"
+                )
+            else:
+                await self.bot.send_message(
+                    chat_id=self.chat_id,
+                    text=formatted["message"],
+                    parse_mode="Markdown"
+                )
+            return "✅ Libro enviado con éxito!"
+        except TelegramError as e:
+            return f"❌ Error al enviar: {e}"
