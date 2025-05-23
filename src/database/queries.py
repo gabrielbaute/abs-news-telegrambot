b065ae0b7d9bae5c2253cd62e7837182e8812c8e
gabrielbaute <gabrielbaute@gmail.com>
2025-05-23 10:35:20 -0400

    Registro de logging de errores en querries

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,3 +1,4 @@
+import logging
 from src.database.db_config import db
 from src.database.models import Book
 
@@ -12,10 +13,20 @@ def add_sent_book(abs_book_data):
         duration=sum(f["duration"] for f in abs_book_data["media"]["audioFiles"]),
         cover_url=f"/api/items/{abs_book_data['id']}/cover" if abs_book_data.get("id") else None
     )
-    db.session.add(book)
-    db.session.commit()
-    return book
+    try:
+        db.session.add(book)
+        db.session.commit()
+        return book
+    except Exception as e:
+        logging.error(f"Error al agregar el libro a la base de datos: {e}", exc_info=True)
+        db.session.rollback()
+        return None
 
 def was_book_sent(abs_id):
     """Verifica si un libro ya fue enviado."""
-    return db.session.query(Book.query.filter_by(abs_id=abs_id).exists()).scalar()
+    try:
+        query = db.session.query(Book.query.filter_by(abs_id=abs_id).exists()).scalar()
+        return query
+    except Exception as e:
+        logging.error(f"Error al verificar si el libro fue enviado: {e}", exc_info=True)
+        return False
