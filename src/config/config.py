439785653e8ba0a845e65c670309da52c5f0c3bc
gabrielbaute <gabrielbaute@gmail.com>
2025-05-23 10:14:12 -0400

    Configuración para base de datos agregada

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,7 @@
 """Variables de configuración del proyecto."""
 import os
 from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool
 
 load_dotenv()
 
@@ -20,6 +21,14 @@ class Config:
     TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
     """Scheduler configuration."""
-    SCHEDULER_TIME_HOUR = os.getenv('SCHEDULER_TIME_HOUR', '14')
-    SCHEDULER_TIME_MINUTE = os.getenv('SCHEDULER_TIME_MINUTE', '54')
+    SCHEDULER_TIME_HOUR = os.getenv('SCHEDULER_TIME_HOUR', '8')
+    SCHEDULER_TIME_MINUTE = os.getenv('SCHEDULER_TIME_MINUTE', '15')
     TZ = os.getenv('TZ', 'America/Caracas')
+
+    """Database configuration."""
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///src/instance/absbot.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    DB_ENGINE_OPTIONS = {
+        'poolclass': NullPool,
+        'connect_args': {'check_same_thread': False} if 'sqlite' in SQLALCHEMY_DATABASE_URI else {}
+    }
