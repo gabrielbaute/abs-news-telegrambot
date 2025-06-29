cffc4b67c2018b605277f2e8c2c49d170bb64a4e
gabrielbaute <gabrielbaute@gmail.com>
2025-06-28 20:53:09 -0400

    scoop transaccional para la db

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,4 +1,4 @@
-from src.database.db_config import db, init_db, Session
+from src.database.db_config import db, init_db, Session, session_scope
 from src.database.models import User, Book
 from src.database.queries import add_sent_book, was_book_sent
 
@@ -6,6 +6,7 @@ __all__ = [
     'db', 
     'init_db', 
     'Session',
+    'session_scope',
     'User', 
     'Book',
     'add_sent_book',
